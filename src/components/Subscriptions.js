a48de120fbba4becf4c5e7294cbe9d81d79bfd07
Jermaine Davis <jermaine@Jermaines-MacBook-Pro.local>
2019-10-29 23:07:49 -0700

    Add functions to manage subscriptions status

--- src/components/Subscriptions.js
+++ src/components/Subscriptions.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+import useSubscriptions from '../hooks/useSubscriptions'
 import styled from 'styled-components'
-import axios from 'axios'
 import Button from './button'
 import Moment from 'react-moment'
+import axios from 'axios'
 
 const Wrapper = styled.div`
   padding: 2rem;
@@ -29,34 +30,98 @@ const Actions = styled.aside`
   }
 `;
 
-const Subscriptions = ({ customer_id }) => {
+const Subscriptions = ({ id }) => {
 
-  const [subscriptions, setSubscriptions] = useState([])
+  const { loaded, subscriptions, fetchData } = useSubscriptions(id)
 
-  useEffect(() => {
-    axios.get(`https://andnone.co/saintcenter/wp-json/wc/v1/subscriptions?customerid=${customer_id}&consumer_key=ck_990f62c74b9f424eb1ecf8b6b1bd3a2b7e180c7a&consumer_secret=cs_0c39f3c5f8db99d8f1493394fffadba7629215cd`)
-    .then(res => {
-      setSubscriptions(res.data)
-    })
-  }, [subscriptions, customer_id])
+  const handleCancel = async (sub_id) => {
+    const data = {
+      status: 'cancelled'
+    }
+    const response = await axios.put(`https://andnone.co/saintcenter/wp-json/wc/v1/subscriptions/${sub_id}/?consumer_key=ck_990f62c74b9f424eb1ecf8b6b1bd3a2b7e180c7a&consumer_secret=cs_0c39f3c5f8db99d8f1493394fffadba7629215cd`, data)
+    if(response.status === 200){
+      fetchData()
+    }
+  }
+
+  const handlePause = async (sub_id) => {
+    const data = {
+      status: 'on-hold'
+    }
+    const response = await axios.put(`https://andnone.co/saintcenter/wp-json/wc/v1/subscriptions/${sub_id}/?consumer_key=ck_990f62c74b9f424eb1ecf8b6b1bd3a2b7e180c7a&consumer_secret=cs_0c39f3c5f8db99d8f1493394fffadba7629215cd`, data)
+      if(response.status === 200){
+        fetchData()
+      }
+  }
+
+  const handleReactivate = async (sub_id) => {
+    const data = {
+      status: 'active'
+    }
+    const response = await axios.put(`https://andnone.co/saintcenter/wp-json/wc/v1/subscriptions/${sub_id}/?consumer_key=ck_990f62c74b9f424eb1ecf8b6b1bd3a2b7e180c7a&consumer_secret=cs_0c39f3c5f8db99d8f1493394fffadba7629215cd`, data)
+      if(response.status === 200){
+        fetchData()
+      }
+  }
+
+  const renderActions = (status, sub_id) => {
+    switch (status) {
+      case 'active':
+        return (
+          <Actions>
+            <Button onClick={() => handlePause(sub_id)}>Pause</Button>
+            <Button onClick={() => handleCancel(sub_id)}>Cancel</Button>
+          </Actions>
+        )
+      case 'cancel':
+        return (
+          <Actions>
+            <Button onClick={() => handleReactivate(sub_id)}>Restart</Button>
+          </Actions>
+        )
+      default:
+        return (
+          <Actions>
+            <Button onClick={() => handleReactivate(sub_id)}>Restart</Button>
+            <Button onClick={() => handleCancel(sub_id)}>Cancel</Button>
+          </Actions>
+        )
+    }
+  }
+
+  const renderStatus = (status, next_payment_date) => {
+    switch (status) {
+      case 'active':
+        return <span>Renews on <Moment format='LL'>{next_payment_date}</Moment></span>
+      case 'cancelled':
+        return <span>Cancelled</span>
+      case 'on-hold':
+        return <span>Paused</span>
+      case 'pending-cancel':
+        return <span>Cancelled</span>
+      case 'expired':
+        return <span>Expired</span>
+      default:
+        return <span></span>
+    }
+  }
 
   return (
     <Wrapper>
-      {subscriptions.length > 0 &&
+      {loaded && subscriptions.length > 0 ?
         subscriptions.map(sub => {
-          const { total, line_items, next_payment_date } = sub;
+          const { total, line_items, next_payment_date, status } = sub;
+          const sub_id = sub.id;
           return (
             <Sub key={sub.id}>
               <span>{line_items[0].name}</span>
               <span>${total} per month</span>
-              <span>Renews on <Moment format='LL'>{next_payment_date}</Moment></span>
-              <Actions>
-                <Button>Pause</Button>
-                <Button>Cancel</Button>
-              </Actions>
+              {renderStatus(status, next_payment_date)}
+              {renderActions(status, sub_id)}
             </Sub>
           )
         })
+        : <span>You have no subscriptions</span>
       }
     </Wrapper>
   )
