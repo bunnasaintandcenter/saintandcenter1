1be4a6cd27194bf5897179a541b153898beea701
Jermaine Davis <jermaine@Jermaines-MacBook-Pro.local>
2019-10-29 23:04:28 -0700

    Added omnisend function to create new contacts

--- functions/omnisend.js
+++ functions/omnisend.js
@@ -1,26 +1,36 @@
-var request = require("request");
+var rp = require("request-promise");
 
-exports.handler = (event, context) => {
+exports.handler = async (event, context) => {
+  try {
 
-  const { email } = JSON.parse(event.body);
-  console.log('email', email)
-  var options = { method: 'POST',
-    url: 'https://api.omnisend.com/v3/contacts',
-    headers:
-     { 'content-type': 'application/json',
-       'x-api-key': '5d6d46aa8653ed0357cf4de1-aH8RW8C9471vhaUzA4LddgiHVSq0X0DhPwjs6BWmzRJBkecdPM' },
-    body:
-     {
-       email: email,
-       status: 'subscribed',
-       statusDate: '2016-02-28T21:17:23Z',
-    },
-    json: true
-  };
+    const { email } = JSON.parse(event.body);
 
-  request(options, function (error, response, body) {
-    if (error) throw new Error(error);
+    var options = { method: 'POST',
+      url: 'https://api.omnisend.com/v3/contacts',
+      headers:
+       { 'content-type': 'application/json',
+         'x-api-key': '5d6d46aa8653ed0357cf4de1-aH8RW8C9471vhaUzA4LddgiHVSq0X0DhPwjs6BWmzRJBkecdPM' },
+      body:
+       {
+         email: email,
+         status: 'subscribed',
+         statusDate: '2016-02-28T21:17:23Z',
+      },
+      json: true
+    };
 
-    console.log(body);
-  });
-}
+    const response = await rp(options)
+
+    console.log(response)
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response)
+    }
+
+  } catch(err) {
+    return {
+      statusCode: 500,
+      body: err.message
+    }
+}}
