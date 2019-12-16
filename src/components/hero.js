c17c8ea01e96352e1a0ea1ae033a0887b869369d
Jermaine Davis <jermaine@Jermaines-MacBook-Pro.local>
2019-12-15 18:44:51 -0800

    Fix underline text-shadow overlap

--- src/components/hero.js
+++ src/components/hero.js
@@ -47,7 +47,7 @@ const Wrapper = styled.div`
         0 -0.03em #eeb805, 0.06em 0 #eeb805, -0.06em 0 #eeb805, 0.09em 0 #eeb805,
         -0.09em 0 #eeb805, 0.12em 0 #eeb805, -0.12em 0 #eeb805, 0.15em 0 #eeb805,
         -0.15em 0 #eeb805;
-      background-position: 0 95%, 100% 95%, 0 95%;
+      background-position: 0 95%, 100% 100%, 0 95%;
     }
 
     @media ${device.laptop} {
