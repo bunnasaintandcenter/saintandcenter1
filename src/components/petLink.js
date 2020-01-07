00e96badf8b4d84e276263b01f9135a92e1d7a17
Jermaine Davis <jermaine@Jermaines-MacBook-Pro.local>
2020-01-07 00:53:43 -0800

    Add new petLink design

--- src/components/petLink.js
+++ src/components/petLink.js
@@ -6,20 +6,13 @@ import styled from "styled-components"
 const Wrapper = styled.div`
   position: relative;
   grid-area: pets;
+  background: ${props => props.theme.color.brown};
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-  .gatsby-image-wrapper {
-    height: 100%;
-  }
-
-  h2 {
-    font-weight: 300;
-    z-index: 1;
-    text-transform: uppercase;
-    font-size: 24px;
-    position: absolute;
-    color: white;
-    top: 24px;
-    left: 24px;
+  a {
+    text-decoration: none;
   }
 
   img {
@@ -31,38 +24,57 @@ const Wrapper = styled.div`
   }
 `
 
-const PetLink = () => {
-  const data = useStaticQuery(graphql`
-    query PetLinkQuery {
-      file: file(relativePath: { eq: "treats.jpg" }) {
-        childImageSharp {
-          fluid {
-            base64
-            tracedSVG
-            aspectRatio
-            src
-            srcSet
-            srcWebp
-            srcSetWebp
-            sizes
-            originalImg
-            originalName
-            presentationWidth
-            presentationHeight
-          }
-        }
-      }
-    }
-  `)
+const Top = styled.div`
+  font-weight: 300;
+  z-index: 1;
+  text-transform: uppercase;
+  font-size: 24px;
+  position: absolute;
+  color: white;
+  top: 24px;
+  left: 24px;
+`
 
-  return (
-    <Wrapper className="petLink">
-      <Link to="/shop/pets">
-        <h2>CBD for Pets</h2>
-        <Img fluid={data.file.childImageSharp.fluid} alt="Pet Products" />
-      </Link>
-    </Wrapper>
-  )
-}
+const Bottom = styled.div`
+  font-weight: 300;
+  z-index: 1;
+  text-transform: uppercase;
+  font-size: 24px;
+  position: absolute;
+  color: white;
+  bottom: 24px;
+  left: 24px;
+`
+
+const Heading = styled.div`
+  color: white;
+  text-transform: uppercase;
+
+  h2 {
+    margin: 0;
+    font-size: 48px;
+  }
+
+  div {
+    display: flex;
+    justify-content: space-between;
+  }
+`
+
+const PetLink = () => (
+  <Wrapper className="petLink">
+    <Link to="/shop/pets">
+      <Top>Explore More</Top>
+      <Heading>
+        <h2>Saint and Center</h2>
+        <div>
+          <h2>For</h2>
+          <h2>Pets</h2>
+        </div>
+      </Heading>
+      <Bottom>Visit</Bottom>
+    </Link>
+  </Wrapper>
+)
 
 export default PetLink
