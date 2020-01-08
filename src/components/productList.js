1183c8c47ddc483eec693c95c4491d0d681cd90b
Jermaine Davis <jermaine@Jermaines-MacBook-Pro.local>
2020-01-08 10:34:41 -0800

    Add id to fixPrice map key

--- src/components/productList.js
+++ src/components/productList.js
@@ -34,6 +34,7 @@ const ProductList = () => {
               price
               product_variations {
                 price
+                id
               }
               images {
                 localFile {
