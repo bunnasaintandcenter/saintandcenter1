042f5700d350cb47c358b076f6de7a3b9c130dde
Jermaine Davis <jermaine@Jermaines-MacBook-Pro.local>
2020-01-12 21:51:31 -0800

    Fix /account logout

--- src/utils/tabs.js
+++ src/utils/tabs.js
@@ -4,7 +4,6 @@ const tabs = [
   { url: "subscriptions", title: "Subscriptions" },
   { url: "payment", title: "Payment" },
   { url: "address-book", title: "Address Book" },
-  { action: () => console.log("logout"), title: "Logout" },
 ]
 
 export default tabs
