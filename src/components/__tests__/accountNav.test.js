853d39547ffe6d0e00f2bdc12efbc7857ef425dc
Jermaine Davis <jermaine@Jermaines-MacBook-Pro.local>
2020-01-14 12:42:39 -0800

    Set up renderWithRedux tests for accountNav

--- src/components/__tests__/accountNav.test.js
+++ src/components/__tests__/accountNav.test.js
@@ -1,25 +1,26 @@
-import React from 'react'
-import { render, cleanup } from '@testing-library/react'
-import AccountNav from '../accountNav'
+import React from "react"
+import { cleanup } from "@testing-library/react"
+import AccountNav from "../accountNav"
+import renderWithRedux from "../../utils/renderWithRedux"
 
 afterEach(cleanup)
 
 const tabs = [
-  { url: 'orders', title: 'Orders'},
-  { url: 'subscriptions', title: 'Subscriptions'},
-  { url: 'payment', title: 'Payment'},
-  { url: 'address-book', title: 'Address Book'},
-  { action: () => console.log('logout'), title: 'Logout'}
+  { url: "orders", title: "Orders" },
+  { url: "subscriptions", title: "Subscriptions" },
+  { url: "payment", title: "Payment" },
+  { url: "address-book", title: "Address Book" },
+  { action: () => console.log("logout"), title: "Logout" },
 ]
 
-it('renders', () => {
-  const { getByTestId } = render(<AccountNav tabs={tabs} />)
-  expect(getByTestId('account-nav')).toBeTruthy()
+it("renders", () => {
+  const { getByTestId } = renderWithRedux(<AccountNav tabs={tabs} />)
+  expect(getByTestId("account-nav")).toBeTruthy()
 })
 
-it('renders tabs titles correctly', () => {
-  const { getAllByTestId } = render(<AccountNav tabs={tabs} />)
-  const renderedTabs = getAllByTestId('tab').map(li => li.textContent)
+it("renders tabs titles correctly", () => {
+  const { getAllByTestId } = renderWithRedux(<AccountNav tabs={tabs} />)
+  const renderedTabs = getAllByTestId("tab").map(li => li.textContent)
   const testTabs = tabs.map(t => t.title)
   expect(renderedTabs).toEqual(testTabs)
 })
