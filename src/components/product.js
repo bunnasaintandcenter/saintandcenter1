7f24f0fb684d03a95b625f5d9c76e99ee055d425
Jermaine Davis <jermaine@Jermaines-MacBook-Pro.local>
2020-01-15 17:52:28 -0800

    Remove janky price sort, sorted on admin

--- src/components/product.js
+++ src/components/product.js
@@ -132,11 +132,7 @@ const Product = ({ name, image, slug, products }) => (
   <Wrapper data-testid="product">
     <Info>
       <h2 data-testid="product-name">{name}</h2>
-      <h3 data-testid="product-price">
-        {showPrice(
-          products.sort((a, b) => (a.menu_order < b.menu_order ? 1 : -1))
-        )}
-      </h3>
+      <h3 data-testid="product-price">{showPrice(products)}</h3>
     </Info>
     <Link to={`/shop/product/${slug}`}></Link>
     {image && (
